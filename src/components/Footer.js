d74019c3111d1caaa6a85430116dd2c7c3f0bf02
Ankush Thakur <atpara99@gmail.com>
2021-06-29 16:30:00 +0530

    UnderLine Remove

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,22 +12,22 @@ const Footer = () => {
             <h5 className="text-uppercase">POLICY</h5>
             <ul className="list-unstyled mb-0">
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   Privacy Policy
                 </a>
               </li>
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   Terms of Use
                 </a>
               </li>
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   Return Policy
                 </a>
               </li>
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   Report Abuse
                 </a>
               </li>
@@ -40,22 +40,22 @@ const Footer = () => {
             <h5 className="text-uppercase">ABOUT US</h5>
             <ul className="list-unstyled mb-0">
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   Brands
                 </a>
               </li>
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   F. A.Qs
                 </a>
               </li>
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   Career
                 </a>
               </li>
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   Career
                 </a>
               </li>
@@ -68,22 +68,22 @@ const Footer = () => {
             <h5 className="text-uppercase">CATEGORIES</h5>
             <ul className="list-unstyled mb-0">
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   Clothing
                 </a>
               </li>
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   Men
                 </a>
               </li>
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   Women
                 </a>
               </li>
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   Kids
                 </a>
               </li>
@@ -96,22 +96,22 @@ const Footer = () => {
             <h5 className="text-uppercase">CONNECT WITH US</h5>
             <ul className="list-unstyled mb-0">
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   Address
                 </a>
               </li>
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   Testimonials
                 </a>
               </li>
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   SiteMap
                 </a>
               </li>
               <li>
-                <a href="#!" className="text-dark">
+                <a href="#" className="text-dark">
                   SiteMap
                 </a>
               </li>
