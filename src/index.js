19b935e313420628516fcd07aa8339863771c846
Ankush Thakur <atpara99@gmail.com>
2021-07-02 14:15:04 +0530

    Hosting fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Home from "./pages/Home/Home";
-import Routes from "./Routes";
+import App from "./App";
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
