8a94223be83d541b907ac931fe70aa26e5a82fd6
Ankush Thakur <atpara99@gmail.com>
2021-07-02 13:49:11 +0530

    Index File Changed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import Home from "./pages/Home/Home";
 import Routes from "./Routes";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+ReactDOM.render(<Home />, document.getElementById("root"));
